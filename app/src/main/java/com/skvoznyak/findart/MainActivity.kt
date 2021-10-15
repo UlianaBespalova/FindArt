02c2931e1855a29e05b22db7a8877931d8c448d1
UlianaBespalova <bespalovau@yandex.ru>
2021-10-15 12:13:38 +0300

    add toolbar

--- app/src/main/java/com/skvoznyak/findart/MainActivity.kt
+++ app/src/main/java/com/skvoznyak/findart/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.skvoznyak.findart
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.skvoznyak.findart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+//        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
