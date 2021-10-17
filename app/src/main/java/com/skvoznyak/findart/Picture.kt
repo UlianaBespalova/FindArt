f6b727f99e0373414a1a235ed08b4c4b2411a67e
UlianaBespalova <bespalovau@yandex.ru>
2021-10-17 21:54:41 +0300

    add screen with results

---
+++ app/src/main/java/com/skvoznyak/findart/Picture.kt
@@ -0,0 +1,4 @@
+package com.skvoznyak.findart
+
+data class Picture(val title:String, val painter:String, val image:Int) {
+}
---
+++ app/src/main/java/com/skvoznyak/findart/PictureAdapter.kt
@@ -0,0 +1,66 @@
+package com.skvoznyak.findart
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+
+
+class PictureAdapter(private val pictures: List<Picture>): Adapter<PictureAdapter.PictureViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.picture, parent, false)
+        return PictureViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+        holder.bind(pictures[position])
+    }
+
+    override fun getItemCount(): Int = pictures.size
+
+
+    class PictureViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private val image:ImageView = itemView.findViewById(R.id.item_picture)
+        private val title:TextView = itemView.findViewById(R.id.item_picture_title)
+        private val painter:TextView = itemView.findViewById(R.id.item_picture_painter)
+
+        fun bind(picture: Picture) {
+            image.setImageResource(picture.image)
+            title.text = picture.title
+            painter.text = picture.painter
+        }
+    }
+}
+
+
+class HeaderAdapter: RecyclerView.Adapter<HeaderAdapter.HeaderViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeaderViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.results_header, parent, false)
+        return HeaderViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HeaderViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+    class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        fun bind() {
+        }
+    }
+}
+
+
+
+
